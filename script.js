a963974f64ed31053c94b074be5d292271c17946
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2022-05-18 22:16:49 -0700

    Second commit, size change works through padding

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mainBtn = document.querySelector('#main-btn');
 const colorBoxes = document.querySelectorAll('.box');
 
 let running = false;
+let count = 0;
 
 function getRandomColor() {
    let letters = "0123456789ABCDEF";
@@ -13,20 +14,34 @@ function getRandomColor() {
    return color;
 }
 
+function getRandomValue() {
+   let padding = "";
+   padding += Math.random() * 3;
+   padding += 'rem';
+   console.log(padding);
+   return padding;
+}
+
 function start() {
    // console.log(colorBoxes);
    if (running) {
       colorBoxes.forEach((box, idx) => {
          if (idx%2 === 0) {
             box.style.background = getRandomColor();
+            box.style.padding = getRandomValue();
          }
          if (idx%5 === 0) {
-            box.style.background = '#00FF00';
+            if (count%2 === 0) {
+               box.style.background = '#000000';
+            } else {
+               box.style.background = '#FFFFFF';
+            }
          }
          if (idx%2 !== 0 && idx%5 !== 0) {
             box.style.background = getRandomColor();
          }
       });
+      count++;
       setInterval(start, 1000);
    }
 }
@@ -35,6 +50,7 @@ mainBtn.addEventListener('click', function() {
    mainBtn.innerText = "STOP";
    if (running) {
       running = false;
+      count = 0;
       mainBtn.innerText = "CHANGE COLORS";
    } else {
       running = true;
